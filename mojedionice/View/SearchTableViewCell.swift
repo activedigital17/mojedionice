6ae73bb7a0fdf95e99438615e7640405adc5b649
activedigital <a>
2021-10-27 15:29:15 +0200

    First commit

---
+++ mojedionice/View/SearchTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTableViewCell.swift
+//  mojedionice
+//
+//  Created by Antonio Plantaš on 27.10.2021..
+//
+
+import Foundation
+import UIKit
+
+class SearchTableViewCell:UITableViewCell{
+    
+    @IBOutlet weak var assetNameLabel:UILabel!
+    @IBOutlet weak var assetSymbolLabel:UILabel!
+    @IBOutlet weak var assetTypeLabel:UILabel!
+    
+    
+}
